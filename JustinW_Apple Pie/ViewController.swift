9a5ce310a70c865ff1b39800139c7e391ee7269e
Justin Wallen <wallenju@gmail.com>
2021-11-11 09:44:16 -0500

    yuh

--- JustinW_Apple Pie/ViewController.swift
+++ JustinW_Apple Pie/ViewController.swift
@@ -13,7 +13,19 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+  
+    // Vars
+    @IBOutlet var letterButtons: [UIButton]!
+    @IBOutlet weak var treeImageLabel: UIImageView!
+    @IBOutlet weak var correctWordLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    //Actions
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        
+        sender.isEnabled = false
+        
+        
+    }
 }
 
